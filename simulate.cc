bdfea152c838bc2339d9f96a31d5908e515fba60
Ziting Shen <zshen@brynmawr.edu>
2016-11-25 16:31:44 -0500

    ???

--- simulate.cc
+++ simulate.cc
@@ -45,7 +45,14 @@ int main(int argc, char *argv[]){
   glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow* (window);
+  GLFWwindow* window = glfwCreateWindow(700, 700, "Make a World", 
+    NULL, NULL);
+  if (!window){
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+  glfwSetWindowPos(window, 100, 0);
+  glfwMakeContextCurrent (window);
 
   glewExperimental = GL_TRUE;
   glewInit();
